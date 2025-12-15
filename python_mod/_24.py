0df1950bc061985e7ed94d492c990391b85eb7db
Luke Clark <lukeclarksf@gmail.com>
2025-12-15 01:00:51 +0100

    Commit

--- python_mod/_24.py
+++ python_mod/_24.py
@@ -22,25 +22,3 @@ class Solution:
             
         return dummy.next
     
-# --- Helper function for list creation and printing ---
-
-def create_linked_list(arr: list[int]) -> ListNode | None:
-    if not arr:
-        return None
-    head = ListNode(arr[0])
-    current = head
-    for val in arr[1:]:
-        current.next = ListNode(val)
-        current = current.next
-    return head
-
-def print_linked_list(head: ListNode | None):
-    if not head:
-        return "[]"
-    
-    nodes = []
-    current = head
-    while current:
-        nodes.append(str(current.val))
-        current = current.next
-    return "[" + " -> ".join(nodes) + "]"
